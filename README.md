a0c44a1aa3a3c3d64b85c2948364a32bb10a438b
tunggary <tunggary2@naver.com>
2021-07-06 15:27:07 +0900

    210706

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# YesToSlide
+# attendace => 출석부
+
+# image => 사진창
--- image.js
+++ image.js
@@ -1,6 +1,6 @@
+//사진 선택하면 주황색 테두리 띄우기
 const activeImage = document.getElementsByClassName("active");
 const image_list = document.getElementsByClassName("image_list");
-
 for (let i = 0; i < image_list.length; i++) {
   image_list[i].addEventListener("click", () => {
     checkRadio();
@@ -13,4 +13,38 @@ function checkRadio() {
       image_list[i].classList.remove("active");
     }
   }
+}
+
+// 모두삭제버튼 누르면 모달 띄우기 & 검은색 뒷배경 띄우기
+const dark_background = document.getElementsByClassName("dark_background");
+const all_delete_btn = document.getElementById("all_delete_btn");
+const all_delete_modal = document.getElementById("all_delete_modal");
+all_delete_btn.addEventListener("click", () => {
+  all_delete_modal.style.display = "block";
+  dark_background[0].style.display = "block";
+});
+
+// 사진삭제버튼 누르면 모달 띄우기 & 검은색 뒷배경 띄우기
+const close_button = document.querySelectorAll(".image_list svg");
+const image_delete_modal = document.getElementById("image_delete_modal");
+for (let i = 0; i < close_button.length; i++) {
+  close_button[i].addEventListener("click", () => {
+    image_delete_modal.style.display = "block";
+    dark_background[0].style.display = "block";
+  });
+}
+
+// 삭제취소버튼 누르면 모달 없애기 & 검은색 뒷배경 없애기
+const delete_btn = document.getElementsByClassName("delete_btn");
+const negative_btn = document.getElementsByClassName("negative_btn");
+const par_back_img = document.getElementsByClassName("par_back_img");
+for (let i = 0; i < delete_btn.length; i++) {
+  delete_btn[i].addEventListener("click", () => {
+    par_back_img[i].style.display = "none";
+    dark_background[0].style.display = "none";
+  });
+  negative_btn[i].addEventListener("click", () => {
+    par_back_img[i].style.display = "none";
+    dark_background[0].style.display = "none";
+  });
 }
