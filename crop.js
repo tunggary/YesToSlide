c1ff65201acd06cae5e6af7aac71c2b332a64083
tunggary <tunggary2@naver.com>
2021-07-09 15:54:05 +0900

    210709

--- crop.js
+++ crop.js
@@ -3,20 +3,44 @@ const send_label = document.getElementById("send_label");
 const image = document.getElementById("image");
 const cropped_data = document.getElementsByClassName("cropped_data");
 const button_UI = document.getElementsByClassName("button_UI");
+const destroy = document.getElementById("destroy");
+const crop = document.getElementById("crop");
+const left_rotate = document.getElementById("left_rotate");
+const right_rotate = document.getElementById("right_rotate");
+let is_working = false;
 
+destroy.addEventListener("click", () => {
+  cropper_destroy();
+});
+
+send_imageOrVideo.addEventListener("click", () => {
+  if (is_working) {
+    cropper_destroy();
+  }
+});
+
+//파일 선택
 send_imageOrVideo.addEventListener("change", function () {
   const file = send_imageOrVideo.files[0];
   const URL_Selected = URL.createObjectURL(file);
-  image.setAttribute("src", URL_Selected);
+
+  //선택된 파일 img파일인지 확인, 아니면 경고창 뜨면서 화면 reload
   if (file.type === "image/jpeg" || file.type === "image/png") {
-    init_func();
+    //선택된 파일 입력, cropper 만들기
+    image.setAttribute("src", URL_Selected);
+    cropper_start();
   } else {
     alert("사진파일을 선택해주세요");
     location.reload();
   }
 });
 
-function init_func() {
+//cropper 만들기
+function cropper_start() {
+  //작업중 true
+  is_working = true;
+
+  //cropper 만들기
   cropper = new Cropper(image, {
     viewMode: 2,
     dragMode: "none",
@@ -26,30 +50,38 @@ function init_func() {
     background: false,
     autoCropArea: 1,
   });
+
+  //buttonUI 보이기
   button_UI[0].style.display = "block";
 }
 
-const destroy = document.getElementById("destroy");
-destroy.addEventListener("click", () => {
+//cropper삭제
+function cropper_destroy() {
+  //cropper삭제
   cropper.destroy();
   cropper = null;
+
+  //loading된 img, cropping된 img삭제
   image.setAttribute("src", " ");
   cropped_data[0].setAttribute("src", " ");
-  button_UI[0].style.display = "none";
-});
 
-const crop = document.getElementById("crop");
+  //buttonUI 숨기기, 작업중 false
+  button_UI[0].style.display = "none";
+  is_working = false;
+}
+
+//crop 버튼 누르면 crop된 사진 보이기
 crop.addEventListener("click", () => {
   const html = cropper.getCroppedCanvas().toDataURL("image/jpeg");
   cropped_data[0].setAttribute("src", html);
 });
 
-const left_rotate = document.getElementById("left_rotate");
+//image 왼쪽으로 회전
 left_rotate.addEventListener("click", () => {
   cropper.rotate(-90);
 });
 
-const right_rotate = document.getElementById("right_rotate");
+//image 오른쪽으로 회전
 right_rotate.addEventListener("click", () => {
   cropper.rotate(90);
 });
