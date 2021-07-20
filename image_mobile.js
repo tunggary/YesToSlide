41a5eede072c4ff0976a3dc14ff70ce050fd2298
tunggary <tunggary2@naver.com>
2021-07-20 13:24:56 +0900

    210720

--- image_mobile.js
+++ image_mobile.js
@@ -1,5 +1,5 @@
 function prepare_set_mobile_screen() {
-  if (is_mobile()) {
+  if (true) {
     const tri = document.getElementsByClassName("tri")[0];
     const image_container = document.getElementsByClassName("image_container")[0];
     const list_content = document.getElementsByClassName("list_content")[0];
@@ -7,7 +7,7 @@ function prepare_set_mobile_screen() {
     tri.style.display = "none"; //모바일이면 imageTab 맨위에 삼각형(화살표) 필요없음
     image_container.style.borderRadius = "0"; // 모바일이면 borderRadius 필요없음
 
-    let screen_height = window.parent.innerHeight - 199; //iframe 외부에 브라우저 높이
+    let screen_height = window.parent.innerHeight - 199; //iframe 외부에 브라우저 높이 - 상단바,하단바 높이
     let gap = 823 - screen_height;
     image_container.style.height = `${823 - gap}px`; // 기존높이 823px
     list_content.style.height = `${673 - gap}px`; // 기존높이 673px
